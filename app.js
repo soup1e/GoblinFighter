8281e35fef5c10b09903594958b63bf9a6525c0e
Sam Adams <sam.adams7785@gmail.com>
2022-09-20 11:47:54 -0700

    Display player and healthbar

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 /* Imports */
 
 /* Get DOM Elements */
-
+const playerHealth = document.getElementById('playerhealth');
+const playerImage = document.getElementById('playerimg');
+let hpBar = document.getElementById('playerhpbar');
 /* State */
-
+let player = {
+    health: 0,
+};
 /* Events */
 
+function displayPlayer() {
+    playerHealth.textContent = Math.max(0, player.health);
+    hpBar.value = playerHealth.textContent;
+    if (player.health < 1) {
+        playerImage.src = 'assets/playerDead.png';
+    } else {
+        playerImage.src = 'assets/player.png';
+    }
+}
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
+displayPlayer();
