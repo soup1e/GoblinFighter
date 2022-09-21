bb17c9e33c251597369306d8773b8b12d6a13c9f
Sam Adams <sam.adams7785@gmail.com>
2022-09-20 17:17:55 -0700

    working mob damage logic

--- app.js
+++ app.js
@@ -48,7 +48,9 @@ const orc = {
     health: 10,
 };
 
-let message = '';
+const playerAttackArray = [0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 8];
+
+let message = 'Start Fighting Goblins!';
 let kills = 0;
 /* Events */
 
@@ -59,8 +61,25 @@ function displayGoblins() {
     for (const goblin of goblins) {
         const goblinEl = renderGoblin(goblin);
         goblinList.append(goblinEl);
+
+        goblinEl.addEventListener('click', () => {
+            if (goblin.health < 1) {
+                message = `He's Already Dead`;
+                displayMessage();
+                return;
+            }
+
+            const playerAttack = getRandomItem(playerAttackArray);
+
+            goblin.health = Math.max(0, goblin.health - playerAttack); // new health is old health minus attack cant go below 0
+
+            displayMessage();
+            displayPlayer();
+            displayGoblins();
+        });
     }
 }
+
 function displayPlayer() {
     playerHealth.textContent = Math.max(0, player.health);
     hpBar.value = playerHealth.textContent;
